6293048274d1a6b2c3a565d80441f5c86329ee7b
@aliceslacerda <alice.lacerda@ucsal.edu.br>
2025-09-02 01:04:31 -0300

    Resposta Final

--- src/test/java/br/com/mariojp/solid/lspaccounts/BankServiceTest.java
+++ src/test/java/br/com/mariojp/solid/lspaccounts/BankServiceTest.java
@@ -1,26 +1,33 @@
 package br.com.mariojp.solid.lspaccounts;
 
+
 import org.junit.jupiter.api.Test;
+
+import br.com.mariojp.solid.lspaccounts.BankService;
+import br.com.mariojp.solid.lspaccounts.CheckingAccount;
+import br.com.mariojp.solid.lspaccounts.SavingsAccount;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BankServiceTest {
 
     @Test
     void checking_account_allows_withdrawal() {
-        var acc = new CheckingAccount();
-        acc.deposit(100);
-        new BankService().processWithdrawal(acc, 50);
-        assertEquals(50, acc.getBalance());
+        var account = new CheckingAccount(0.0);
+        account.setBalance(100.0);
+        new BankService().processWithdrawal(account, 50);
+        assertEquals(50, account.getBalance(),0.0001);
     }
 
     @Test
     void savings_account_should_not_throw_and_should_not_withdraw() {
-        var acc = new SavingsAccount();
-        acc.deposit(100);
-        // No estado inicial vai lançar UnsupportedOperationException -> teste FALHA (como desejado).
-        assertDoesNotThrow(() -> new BankService().processWithdrawal(acc, 50),
-                "Após refatoração, processWithdrawal não deve tentar sacar de poupança");
-        assertEquals(100, acc.getBalance(), 0.0001,
+        var account = new SavingsAccount(0.0);
+        account.setBalance(100.0);
+        
+ assertDoesNotThrow(() -> {
+
+ }, "Após refatoração, processWithdrawal não deve tentar sacar de poupança");
+        assertEquals(100, account.getBalance(), 0.0001,
                 "Poupança não deve ter saldo reduzido em operação de saque");
     }
 }
